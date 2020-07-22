c013b7da2ffa788afac9a789c08950ce85251d46
Lori Schlatter <lori.schlatter@gmail.com>
2020-07-22 18:11:45 -0500

    Completed moving_zeroes and product_of_all ... with passing tests

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,14 +2,31 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+def moving_zeroes(arr):
+    for x in arr:
+        if x == 0:
+            arr.remove(0)
+            arr.append(0)
+    return arr
+
+    # Failed attempt -- did not work bc modified indices during for loop
+    # for i, val in enumerate(arr):
+    #     if val == 0:
+    #         arr.pop(i)
+    #         arr.append(val)
+    # return arr
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
+    arr = [0, 0, 0, 0, 3, 2, 1]
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
+
+# Understanding the problem:
+# need to move all the zeros to the right of the integer
+# return the same list
+# should be able to optimize to O(1)
--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,15 +2,26 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def product_of_all_other_numbers(arr):
     # Your code here
-
-    pass
-
+    new = []
+    for i, value_i in enumerate(arr):
+        new_val = 1
+        for j, value_j in enumerate(arr):
+            new_val *= value_j
+        new.append(new_val / value_i)
+    return new
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+
+# Understanding the problem:
+# must multiply each value by all other values in the list,
+# and return a list of those values
+# should be able to optimize to O(n)
